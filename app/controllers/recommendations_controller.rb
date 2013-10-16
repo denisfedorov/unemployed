f79303945fa4055ad8ad194d86ba137c01c3deda
Denis Fedorov <fedorov.denis@gmail.com>
2013-10-17 01:32:29 +0400

    Saving new recommendation and service

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,9 +1,21 @@
+# encoding: UTF-8
 class RecommendationsController < ApplicationController
   before_filter :signed_in_user
 
   def new
   	@service = Service.new
   	@recommendation = current_user.recommendations.new(:service_id => @service.id)
+  end
+
+  def create
+  	@service = Service.new(params[:service])
+  	if @service.save
+  		@recommendation = current_user.recommendations.new(:service_id => @service.id)
+  		@recommendation.save
+      flash[:success] = "Спасибо! Рекомендация добавлена."
+      redirect_to @current_user
+  	end
+
   end
 end
 
--- spec/requests/recommendation_pages_spec.rb
+++ spec/requests/recommendation_pages_spec.rb
@@ -14,6 +14,24 @@ describe "Recommendation pages" do
     it { should have_title('Новая рекомендация') }
     it { should have_xpath("//input[@value='Добавить']") }
 
+	  describe "new recommendation" do
+    	let(:submit) { "Добавить" }
+	    before do
+	      fill_in "Услуга",         with: "Женская стрижка"
+	      fill_in "Имя",        with: "Example User"
+	      fill_in "Номер телефона",     with: "89038488888"
+	      fill_in "E-mail", with: "user@example.com"
+	      fill_in "Описание услуги", with: "Стрижка, окраска и все такое..." 
+	    end
+
+	    it "should create a service" do
+	      expect { click_button submit }.to change(Service, :count).by(1)
+	    end
+	    it "should create a recommendation" do
+	      expect { click_button submit }.to change(Recommendation, :count).by(1)
+	    end
+	  end
+
   end
 end
 
